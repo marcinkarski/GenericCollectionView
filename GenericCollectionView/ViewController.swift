7c1b5337d659371b446473543b82805535dc4b11
Marcin Karski <karski80@gmail.com>
2018-11-03 21:07:12 +0000

    Initial Commit

---
+++ GenericCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericCollectionView
+//
+//  Created by Marcin Karski on 03/11/2018.
+//  Copyright © 2018 Marcin Karski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
